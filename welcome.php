ce34281036c3c263f99b672bdbdb00b8aeb41e52
Pablo Reszczynski <pablore@me.com>
2015-10-06 01:33:42 -0300

    new welcome.php

---
+++ welcome.php
@@ -0,0 +1,14 @@
+<html lang="es">
+  <head>
+    <title>welcome</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <div class="container">
+      <?php
+        echo "<h1> Bienvenido " $_SESSION["user"] "!</h1>";
+      ?>
+    </div>
+
+  </body>
+</html>
