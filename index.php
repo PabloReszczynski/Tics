3d7651e074a50cab53384954533a054b8d05770c
Pablo Reszczynski <pablore@me.com>
2015-10-06 00:04:43 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<html lang="es">
+  <head>
+    <title>placeholder</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <div class="container" style="text-align:center;">
+      <h1>Bienvenido</h1>
+      <form action="" method="post">
+        <div class="form-group">
+          <input type="text" name="username" placeholder="Nombre de usuario">
+        </div>
+        <div class="form-group">
+          <input type="password" name="pass" placeholder="Contraseña">
+        </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+      </form>
+    </div>
+  </body>
+</html>
