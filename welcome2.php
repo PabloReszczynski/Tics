eff3008e384456aea5c7e04dfc8384499f154d37
PuduMagico <nhigueraruiz@gmail.com>
2015-10-07 12:35:48 -0300

    Update welcome2.php

--- welcome2.php
+++ welcome2.php
@@ -85,6 +85,13 @@
 				<li><a href="">Contacto</a></li>
 			</ul>
 		</div>
+    <div>
+    	<ul class="nav nav-pills nav-stacked">
+  <li role="presentation" class="active"><a href="#">Home</a></li>
+  <li role="presentation"><a href="#">Profile</a></li>
+  <li role="presentation"><a href="#">Messages</a></li>
+	</ul>	
+	</div>
     <div class="container">
       <?php
         echo "<h1> Bienvenido user!</h1>";
