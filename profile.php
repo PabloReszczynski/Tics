2464df61fd72c0bff8384af4396e0653298a6542
Pauliescobar <Pauli.escobar@yahoo.cl>
2015-10-07 13:28:46 -0300

    pagina de perfil
    sin avatar aun!

--- profile.php
+++ profile.php
@@ -1,95 +1,34 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<meta charset=“UTF-8”>
-		<title>Mi Perfil</title>
-	</head>
-	<body>
-<?php
-session_start();
-if (!isset($_SESSION['user'])){
-	header("Location: index.php");
-	}
-?>
-<!--titulo -->
-<h3>Mi Perfil</h3>
-<br/>
-<!--tabla con datos personales -->
-<table border="1px">
-			<tr>
-				<td>
-					Nombre
-				</td>
-				<td>
-					Cosita Escobar
-				</td>
-				<td>
-					<form method="POST" action="miperfil.php"> <!--opcion a actualizar datos -->
-					Actualizar nombre: <input type="text"/>
-					<input type="submit" value="Enviar" />
-					</form>
-				</td>
-			</tr>
-			
-			<tr>
-				<td>
-					Sexo
-				</td>
-				<td>
-					Femenino
-				</td>
-				<td>
-					<form method="POST" action="miperfil.php"> <!--opcion a actualizar datos -->
-					Actualizar sexo: <input type="text"/>
-					<input type="submit" value="Enviar" />
-					</form>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					Especie
-				</td>
-				<td>
-					Felina
-				</td>
-				<td>
-					<form method="POST" action="miperfil.php"> <!--opcion a actualizar datos -->
-					Actualizar especie: <input type="text"/>
-					<input type="submit" value="Enviar" />
-					</form>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					Fecha de nacimiento
-				</td>
-				<td>
-					15 de octubre, 2012
-				</td>
-				<td>
-					<form method="POST" action="miperfil.php"> <!--opcion a actualizar datos -->
-					Actualizar fecha de nacimiento: <input type="text"/>
-					<input type="submit" value="Enviar" />
-					</form>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					Dirección
-				</td>
-				<td>
-					Aconcagua 620, Reina Sur, Colina
-				</td>
-				<td>
-					<form method="POST" action="miperfil.php"> <!--opcion a actualizar datos -->
-					Actualizar dirección: <input type="text"/>
-					<input type="submit" value="Enviar" />
-					</form>
-				</td>
-			</tr>	
-		</table>	
-<br>
-<!--volver a pagina principal -->
-<a href="principal.php">Volver  pagina principal</a>
-	</body>
+<head>
+	<title>Usuario</title>
+</head>
+
+<body>
+    <h1>Mi Perfil</h1>
+     
+<div align= "center">
+
+ <form >
+       <table>
+        <tr>
+         <td>Nombre completo:</td> <td>Paulina Escobar</td> <td><input type="text" name="name"></td><br>
+        </tr>
+        <tr>
+         <td>Nombre de usuario:</td> <td>pauli.escobar</td> <td><input type="text" name="email"></td><br>
+        </tr>
+        <tr>
+         <td>Número Telefónico:</td> <td>9.3255332</td> <td><input type="password" name="pwd"></td><br>
+        </tr>
+        <tr>
+         <td>E-mail de contacto :</td> <td>pauli.escobar@yahoo.cl</td> <td><input type="password" name="cpwd"><br>
+        </tr>
+        <tr>
+  <td></td> <td><input type="submit" value="Submit"></td>
+        </tr>
+        
+       </table>
+      </form>
+</div>
+</body>
 </html>
